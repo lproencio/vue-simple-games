6e8b1e1a125d02283f2995bf33c6f3e58d9b5e26
lproencio <leandroproencio@gmail.com>
2021-06-01 21:06:04 -0300

    init project

---
+++ src/components/MemoryGame.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="memory_game">
+    <h1>memory_game</h1>
+    <card_memory />
+  </div>
+</template>
+
+<script>
+import card_memory from "@/components/card/Memory";
+
+export default {
+  name: "HelloWorld",
+
+  components: {
+    card_memory,
+  },
+};
+</script>
---
+++ src/components/card/Memory.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card_memory">
+    <div class="card">Front</div>
+    <div class="card">Back</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "card_memory",
+  setup() {},
+};
+</script>
